769be524947606cbfd77364170f2eb3c72633521
milk-abc <3292944779@qq.com>
2021-09-27 11:22:39 +0800

    feat: 前端基础

--- front-end/practice.js
+++ front-end/practice.js
@@ -1,8 +1,31 @@
-var a = 1;
-function b() {
-  a = 10;
-  return;
-  function a() {}
+class EventBus {
+  constructor() {
+    this.events = {};
+  }
+  on(event, callback) {
+    let callbacks = this.events[event] || [];
+    callbacks.push(callback);
+    return this;
+  }
+  emit(args) {
+    let event = args[0];
+    let params = [].slice.call(args, 1);
+    let callbacks = this.events[event];
+    callbacks.forEach((callback) => callback.apply(this, params));
+    return this;
+  }
+  off(event, callback) {
+    let callbacks = this.events[event];
+    if (callbacks) {
+      this.events[event] = this.callbacks.filter((fn) => fn !== callback);
+    }
+    return this;
+  }
+  once(event, callback) {
+    let fn = (...args) => {
+      callback.apply(this, args);
+      this.off(event, callback);
+    };
+    this.on(event, fn);
+  }
 }
-b();
-console.log(a);
