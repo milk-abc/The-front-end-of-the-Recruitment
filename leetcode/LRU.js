af47096e28305c0f5d8af2b4d923d2acaf5f7c3d
milk-abc <3292944779@qq.com>
2024-03-11 00:23:19 +0800

    feat(react): react 基础

---
+++ leetcode/LRU.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function (capacity) {
+  this.capacity = capacity;
+  this.map = new Map();
+};
+
+/**
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function (key) {
+  if (this.map.has(key)) {
+    const value = this.map.get(key);
+    this.map.delete(key);
+    this.map.set(key.value);
+    return value;
+  } else {
+    return -1;
+  }
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function (key, value) {
+  if (this.map.has(key)) {
+    this.map.delete(key);
+    this.map.set(key, value);
+  } else {
+    this.map.size === this.capacity
+      ? this.map.delete(this.map.keys().next().value)
+      : null;
+    this.map.set(key, value);
+  }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
