4f7eeb106a13a1ed810f687221b78a56eec07ac1
milk-abc <3292944779@qq.com>
2024-04-07 10:39:04 +0800

    fix(basic): basic

--- front-end/前端工程化/打包/webpack相关/TPL-LOADER-CREATOR/loaders/tpl-loader/index.js
+++ front-end/前端工程化/打包/webpack相关/TPL-LOADER-CREATOR/loaders/tpl-loader/index.js
@@ -1,9 +1,15 @@
 const { tplReplace } = require("../utils");
+const { getOptions } = require("loader-utils");
 function tplLoader(source) {
   source = source.replace(/\s+/g, "");
+  const { log } = getOptions(this);
+  const _log = log
+    ? `console.log('compiled the file which is from ${this.resourcePath}')`
+    : "";
   console.log("source", source);
   return `export default (options)=>{
 		${tplReplace.toString()}
+    ${_log.toString()}
   	return tplReplace('${source}',options)
   }`;
 }
