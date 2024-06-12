f932947d8c20cd0e08dcb1b8dcf55bfeb76e1840
milk-abc <3292944779@qq.com>
2024-06-12 21:52:33 +0800

    fix(deepcopy): deepcopy

--- front-end/Javascript基础/常用函数底层源码/deepcopy.js
+++ front-end/Javascript基础/常用函数底层源码/deepcopy.js
@@ -2,7 +2,13 @@
 //深拷贝：json.parse(json.stringify())，
 //json.stringify在对象中遇到undefined,symbol,function会自动忽略，在数组则会返回null
 //为了解决循环引用问题，需要一个额外的数据结构来跟踪已经复制过的对象
-let deepcopy = function (obj, visited = new WeakMap()) {
+/**
+ * 
+Object.keys()  方法会返回一个给可见定对象的自身可枚举属性组成的数组。
+Reflect.ownKeys()  返回一个由目标对象自身的属性键组成的数组(所有属性)。
+Object.getOwnPropertyNames() 方法返回一个由指定对象的所有自身属性的属性名（包括不可枚举属性但不包括 Symbol 值作为名称的属性）组成的数组。
+ */
+let deepCopy = function (obj, visited = new WeakMap()) {
   if (typeof obj !== "object" || obj === null) {
     // 只拷贝对象
     return obj;
@@ -14,11 +20,13 @@ let deepcopy = function (obj, visited = new WeakMap()) {
   let newObj = obj instanceof Array ? [] : {};
   visited.set(obj, newObj);
   // 遍历obj，并且判断是obj的属性才拷贝
-  for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      newObj[key] =
-        typeof obj[key] === "object" ? deepcopy(obj[key], visited) : obj[key];
-    }
+  for (let key of Reflect.ownKeys(obj)) {
+    newObj[key] =
+      typeof obj[key] === "object" ? deepCopy(obj[key], visited) : obj[key];
   }
   return newObj;
 };
+let a = { a: 1 };
+let b = deepCopy(a);
+b["a"] = 3;
+console.log("deepCopy", a, b);
